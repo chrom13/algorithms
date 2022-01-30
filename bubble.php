4cfb2461dee83bbd1335455105f5213b03165ad7
christianlopez <christian_lopez@avansys.com.mx>
2022-01-29 23:36:37 -0600

    Bubble sort

---
+++ bubble.php
@@ -0,0 +1,22 @@
+<?php
+function bubbleSort($arr)
+{
+    $aux=null;
+    for($i=0;$i<count($arr)-1;$i++)
+    {
+        for($j=0;$j<count($arr)-$i-1;$j++)
+        {
+            if($arr[$j+1]<$arr[$j])
+            {
+                $aux=$arr[$j+1];
+                $arr[$j+1]=$arr[$j];
+                $arr[$j]=$aux;
+            }
+        }
+
+    }
+    return $arr;
+}
+
+
+print_r(bubbleSort(array(5,23,43,1,2,67,7,8,4,5,23)));
